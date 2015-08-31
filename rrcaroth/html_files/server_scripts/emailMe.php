ca27bb81a5a3ad21784357269ef41901de387693
George P. Corser <gpcorser@csis.svsu.edu>
2015-08-31 08:24:04 -0400

    initialization of cis255 2015fall

--- rrcaroth/html_files/server_scripts/emailMe.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "rrcaroth@svsu.edu";
-$subject = "First PHP email";
-$txt = "Hello world!";
-
-mail($to,$subject,$txt);
-?>
--- rrcaroth/scripts/mod-list.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("tr").mouseenter(function(){
-			$("tr").addClass("highlight");
-		});
-    $("tr").mouseleave(function(){
-			$("table tr").removeClass("highlight");
-		});
-});
