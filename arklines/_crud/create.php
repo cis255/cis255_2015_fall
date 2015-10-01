db00997e634e33db40332ae82a385da097beb6cb
Allison R. Klinesmith <arklines@csis.svsu.edu>
2015-10-01 08:34:21 -0400

    arklines: update

--- arklines/_crud/create.php
+++ arklines/_crud/create.php
@@ -1,4 +1,20 @@
-<?php 
+<?php
+// Author: Allison Klinesmith
+// Purpose: to test front end design with provided, back end code, and to test understanding of CSS and bootstra 
+// Design:
+//The specs are below.
+//
+//1. Copy the entire "_crud" directory recursively into your subdirectory. For example, after you position yourself in the correct directory, type "mkdir _crud" and "cp –R ../_crud01/* _crud". If you don't know how to copy files and directories in Linux, please learn this. This was supposed to be a prerequisite for this course. If you can't figure it out on your own, please come to my office hours ASAP!!!
+//2. Inside the subdirectory, you will find 6 files with the extension, .php. You will NOT change database.php, but you will change the other 5 files. Change the HTML/CSS part of the code, not the PHP part of the code (unless you want extra credit). Use your own custom CSS/Bootstrap.
+//3. Use relative referencing and absolute referencing when linking to external CSS files. Use double periods ".." in at least one of your references and put a numbered comment next to it so I can find it.
+//4. Use in-line, internal (embedded) and external CSS.
+//5. Use at least 20 Bootstrap classes and comment them to show you understand what they do.
+//6. Use the "!important" attribute to override a Bootstrap class.
+//7. Use at least one of each of the following: an ID selector, a CLASS selector, an ELEMENT selector, an attribute selector and a pseudo selector.
+//8. Comment in your code at least one of each of the following: how your CSS execution is affected by (a) inheritance, (b) specificity, and (c) location.
+//9. Add an image to each of the 5 files.
+//10. Change all buttons to include appropriate glyph-icons.
+
 	
 	require 'database.php';
 
@@ -51,16 +67,19 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link   href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- #3 absolute referencing -->
+   <link   href="http://csis.svsu.edu/~arklines/cis255/arklines/_crud/css/bootstrap.min.css" rel="stylesheet">
+	
     <script src="js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+	
 	<!-- Meta tag makes website mobile-first-->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
+
     <!-- class container creates a fixed width container. Another option is fluid-container, which will dynamically fill the view -->
-    <div class="container">
+    <div class="container"> 
     
     			<div class="span10 offset1">
     				<div class="row">
@@ -103,5 +122,6 @@
 				</div>
 				
     </div> <!-- /container -->
+	
   </body>
 </html>
