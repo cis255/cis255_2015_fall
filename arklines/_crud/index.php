04a7c557d525e2eed7e19cb4f505f5b2c338f66e
Allison R. Klinesmith <arklines@csis.svsu.edu>
2015-10-05 09:35:57 -0400

    update arklines

--- arklines/_crud/index.php
+++ arklines/_crud/index.php
@@ -29,31 +29,26 @@
     <meta name="author" content="">
 	
 	
-
+	
     <title>Prog2- Allison Klinesmith</title>
-
     <!-- Bootstrap Core CSS -->
 	<!-- #3 Relative reference -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom CSS -->
 	<!-- #3; ".." notation relative referencing-->
     <link href="../_crud/css/simple-sidebar.css" rel="stylesheet">
-	
 	<!-- #7 element selector. #6 !important attribute to override bootstrap. #4 internal -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/bootstrap.jquery"></script>
 	<style>
-	
 		h3{font-weight: bold !important;}
-	</style>
-
-	
+	</style>	
 </head>
 
 
-
 <body>
-
-<div id="wrapper">
+	<div id="wrapper">
 		<!-- CSS and wrapper code from StartBootstrap-simple-sidebar-->
         <!-- Sidebar -->
 		<!-- #4 inline styling; inline overrides CSS bootstrap -->
@@ -80,34 +75,27 @@
                 <li>
                     <a href="http://startbootstrap.com/template-overviews/simple-sidebar/">Template Used</a>
                 </li>
-                
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
 
 
-		
-		
-		<!-- content -->
-		<!--  wraps content in format of the content for the simple sidebar -->
+	<!-- content -->
+	<!--  wraps content in format of the content for the simple sidebar -->
 	<div id="page-content-wrapper">
-	<!-- #5 (2) content of the page is fluid so that it adapts to the collapsible sidebar-->
-    <div class="container-fluid">
-				<!-- #5 (3) added class to format header-->
-    			<h3 class="header page-header">CIS 255 Email List</h3>
-				<!-- ID Puprose will be changed via an id selector in bootstrap.CSS -->
-                <p id="Purpose">The purpose of this list is to provide a back end to test front end techniques.</p>
-    		
+		<!-- #5 (2) content of the page is fluid so that it adapts to the collapsible sidebar-->
+		<div class="container-fluid">
+			<!-- #5 (3) added class to format header-->
+    		<h3 class="header page-header">CIS 255 Email List</h3>
+			<!-- ID Puprose will be changed via an id selector in bootstrap.CSS -->
+            <p id="Purpose">The purpose of this list is to provide a back end to test front end techniques.</p>
 			<!-- #5 (4) formats row column and margin sizes-->
 			<div class="row">
 				<p>
-					<!-- #5 (5) changed class from button success to button primary. Primary button in bootstrap has specs background-color:#337ab7;border-color:#2e6da4-->
-					<!-- Trigger the modal with a button; Code from example in Weebly site -->
-					<a class="btn btn-primary" href="create.php" > Create</a>
-					
+					<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">Create</button>
 				</p>
-				<!-- #5 (6) table-bordered-thick gives a 2px shadow to the bottom-right sides of the table -->
-				<table class="table table-striped table-bordered-thick">
+				
+				<table class="table table-striped table-bordered">
 		              <thead>
 		                <tr>
 		                  <th>Name</th>
@@ -133,38 +121,50 @@
 							   	echo '<a class="btn btn-success" 
 								   href="update.php?id='.$row['id'].'">Update</a>';
 							   	echo '&nbsp;';
-								/*Addition of title gives rollover text warning in php*/
-							   	echo '<a class="btn btn-danger"
-								   href="delete.php?id='.$row['id'].'" title="WARNING: You may not have the required access for this">Delete</a>';
+							   	echo '<a class="btn btn-danger" 
+								   href="delete.php?id='.$row['id'].'">Delete</a>';
 							   	echo '</td>';
 							   	echo '</tr>';
 					   }
 					   Database::disconnect();
 					  ?>
-					  
 				      </tbody>
 	            </table>
 				<!-- changed tutorial button from class success to class info, which is normally used for links.-->
 				<a href="http://www.startutorial.com/articles/view/php-crud-tutorial-part-1" class="btn btn-info" role="button">Tutorial</a>
 				<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-    	</div>
+			</div> <!--row -->
+			
+			
 		
-		
-		
-		 
 
- 
-	
-	
-	
-</div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+			
+			
+		</div> <!-- container fluid -->
+	</div>
+    <!-- /#content wrapper -->
+</div> <!-- wrapper>
+   
 
     <!-- Menu Toggle Script -->
     <script>
@@ -172,17 +172,8 @@
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-	
-	
-	
-	
   
-  </script>
+    </script>
 	
-    </script>	
-	
-	
-	
-
-  </body>
+ </body>
 </html>
