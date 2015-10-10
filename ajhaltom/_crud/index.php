51e3884122a7c98693573b34a689e308b7fc63cd
Alex J. Haltom <ajhaltom@csis.svsu.edu>
2015-10-10 01:20:49 -0400

    ajhaltom:update

--- ajhaltom/_crud/index.php
+++ ajhaltom/_crud/index.php
@@ -1,23 +1,40 @@
+<!--
+filename   : index.php
+author     : Alex Haltom (CIS-255, Fall 2015)
+description: This program is a part of _crud. Specifically for viewing all entries at the same time.
+design     : 1. Title
+			 2. _crud view
+			 3. Background image from external css with !important attribute!
+comments   : Comments include description of Bootstrap classes, inline and internal CSS, referencing examples
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link   href="css/bootstrap.min.css" rel="stylesheet"> -->
+	<!-- 3. Example of absolute referencing --> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- 3. Example of relative referencing -->
+	<!-- 4. External CSS) -->
+	<link rel="stylesheet" href="../css/ajhaltom_prog2style.css"
     <script src="js/bootstrap.min.js"></script>
 </head>
-
+<!-- 4. Internal CSS -->
+<style>
+body {background-image: url("http://images.alphacoders.com/538/53823.jpg");}
+</style>
 <body>
-    <div class="container">
+    <div class="container"> <!-- Container: Coding is done inside container in order to receive bootstrap -->
     		<div class="row">
-    			<h3>CIS 255 Email List</h3>
+    			<!-- 4. Example of inline --><h3 style="color: blue;">CIS 255 Email List</h3>
                 <p>The purpose of this list is to provide a back end to test front end techniques.</p>
     		</div>
-			<div class="row">
+			<div class="row"> <!-- Row: Creates row -->
 				<p>
-					<a href="create.html" class="btn btn-success">Create</a>
+					<a href="create.php" class="btn btn-success">Create</a> <!-- btb btn-success: Creates clickable button -->
 				</p>
 				
-				<table class="table table-striped table-bordered">
+				<table class="table table-striped table-bordered"> <!-- table: creates table -->
 		              <thead>
 		                <tr>
 		                  <th>Name</th>
