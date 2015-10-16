5b13690c9e3148ce712938693db670e19ea9d773
Tessa F. Genovese <tfgenove@csis.svsu.edu>
2015-10-16 10:30:22 -0400

    tfgenove: exercise 4

--- tfgenove/tfgenove_exercise04.js
+++ tfgenove/tfgenove_exercise04.js
@@ -1,1 +1,38 @@
 alert('3. You should click Ok');
+console.log("finished alert #3")
+/* assigning function to variable (first class function) - object*/
+var car = function(color,sound) {
+	this.color = color;
+	this.sound = sound;
+	};
+car.prototype.horn = function () {
+	document.write('this car goes ' + this.sound + "<br />");
+	};
+toyota = new car("red","beep");
+buick = new car("green","honk");
+
+toyota.horn();
+buick.horn();
+
+try {
+	nonexistentfunction("not here");
+}
+catch (e){
+	alert("exception caught!!")
+}
+
+document.getElementById("container").innerHTML = "zippy";
+
+months["Jan", "Feb", "Mar", "Apr", "May"];
+
+monthsHTML = "";
+monthsHTML = monthsHTML + "<ol>";
+
+for (i=0;i<months.length;i++){
+	monthsHTML = monthsHTML + "<li>";
+	monthsHTML = monthsHTML + months[i];
+	monthsHTML = monthsHTML + "</li>";
+}
+
+monthsHTML = monthsHTML + "</ol>";
+document.getElementById("container").innerHTML = monthsHTML;
