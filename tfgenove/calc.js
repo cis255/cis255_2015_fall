bbea216de82ad056f6fa131e61bf6370a62b0ec5
Tessa F. Genovese <tfgenove@csis.svsu.edu>
2015-12-07 09:03:26 -0500

    tfgenove: final

---
+++ tfgenove/calc.js
@@ -0,0 +1,4 @@
+function displayNum(n1)
+{
+calcform.txt1.value=calcform.txt1.value+n1; //Takes the text from the text box, turns it into a value, and "adds" to it whatever    arithmetic function is assigned to it along with the clicked button's corresponding text number
+}
